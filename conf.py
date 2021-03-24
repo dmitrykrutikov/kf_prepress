d5049bbc3dbdcb0c1bdb09abc78927c55515cb27
dmitrykrutikov <dimakrut16@gmail.com>
2021-03-24 13:40:42 +0300

    initial commit

--- conf.py
+++ conf.py
@@ -44,11 +44,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import rtcat_sphinx_theme
-
-html_theme = "rtcat_sphinx_theme"
-
-html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
